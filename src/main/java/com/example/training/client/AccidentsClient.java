bfbd048c62f8c163fd57ff7121a2f1edf68270e5
jcandelas94 <jcandelas@izertis.com>
2024-09-03 13:26:52 +0200

    added circuitbreaker

--- src/main/java/com/example/training/client/AccidentsClient.java
+++ src/main/java/com/example/training/client/AccidentsClient.java
@@ -1,6 +1,7 @@
 package com.example.training.client;
 
 import com.example.training.model.entity.Accident;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import java.util.List;
 @FeignClient(name = "accidentsClient", url = "${accidents.service.url}")
 public interface AccidentsClient {
 
+    @CircuitBreaker(name = "accidents")
     @GetMapping("/polizas/{policyId}/siniestros")
     List<Accident> getAccidentsByPolicy(@PathVariable("policyId") String policyId);
 
